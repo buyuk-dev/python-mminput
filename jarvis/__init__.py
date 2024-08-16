b4aec267d183d7b07af651206dcc703c5538c657
Michał Michalski <michal@buyuk-dev.com>
2024-08-16 19:42:11 +0200

    Surpress pytorch warning on import;

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -1,1 +1,6 @@
+import warnings
+
+# Suppress specific FutureWarning
+warnings.filterwarnings("ignore", category=FutureWarning, message=".*torch.load.*weights_only=False.*")
+
 from .interface import VoiceRecorder, Transcriber, VoiceInput, audio_input
